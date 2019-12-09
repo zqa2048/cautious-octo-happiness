c163d87b7c490d6287d7585ca014ff644011b580
周乾 <2603682659@qq.com>
2019-12-09 23:19:11 +0800

    ts 定义函数的种类及方法（正常、可选参数、默认参数，以及剩余参数）

---
+++ demo03.js
@@ -0,0 +1,51 @@
+"use strict";
+function search(age) {
+    return '找到了' + age + '岁的小姐姐！';
+}
+var num = 18;
+var result = search(num);
+console.log(result);
+function search2(age, stature) {
+    var yy;
+    yy = '找到了' + age + '岁';
+    if (stature != undefined) {
+        yy = yy + stature;
+    }
+    return yy + '的小姐姐';
+}
+var result = search2(num);
+console.log(result);
+result = search2(num, '大长腿');
+console.log(result);
+console.log('------------');
+function search3(age, stature) {
+    if (age === void 0) { age = 18; }
+    if (stature === void 0) { stature = '水蛇腰'; }
+    var yy;
+    yy = '找到了' + age + '岁';
+    if (stature != undefined) {
+        yy = yy + stature;
+    }
+    return yy + '的小姐姐';
+}
+result = search3();
+console.log(result);
+result = search3(17, '大波浪');
+console.log(result);
+console.log('-------------');
+function search3() {
+    var xuqiu = [];
+    for (var _i = 0; _i < arguments.length; _i++) {
+        xuqiu[_i] = arguments[_i];
+    }
+    var yy = '找到了';
+    for (var i = 0; i < xuqiu.length; i++) {
+        yy = yy + xuqiu[i];
+        if (i < xuqiu.length - 1) {
+            yy = yy + '、';
+        }
+    }
+    return yy + '的小姐姐';
+}
+result = search3('16岁', '瓜子脸', '大波浪', '水蛇腰', '大长腿');
+console.log(result);
---
+++ demo03.ts
@@ -0,0 +1,51 @@
+function search(age:number):string{
+    return '找到了'+age+'岁的小姐姐！'
+}
+var num:number = 18
+var result:string = search(num)
+
+console.log(result)
+
+function search2(age:number,stature?:string):string{
+    let yy:string
+    yy = '找到了'+age+'岁'
+    if(stature!=undefined){
+        yy = yy + stature
+    }
+    return yy+'的小姐姐'
+}
+var result=search2(num)
+console.log(result)
+result = search2(num,'大长腿')
+console.log(result)
+console.log('------------')
+
+function search3(age: number=18, stature?: string='水蛇腰'): string {
+    let yy: string
+    yy = '找到了' + age + '岁'
+    if (stature != undefined) {
+        yy = yy + stature
+    }
+    return yy + '的小姐姐'
+}
+result=search3()
+console.log(result)
+result = search3(17,'大波浪')
+console.log(result)
+
+
+console.log('-------------')
+
+
+function search3(...xuqiu:string[]): string {
+    let yy: string='找到了'
+    for (let i=0;i< xuqiu.length;i++){
+        yy = yy + xuqiu[i]
+        if (i<xuqiu.length-1){
+            yy = yy + '、'
+        }
+    }
+    return yy + '的小姐姐'
+}
+result = search3('16岁','瓜子脸','大波浪','水蛇腰','大长腿')
+console.log(result)
